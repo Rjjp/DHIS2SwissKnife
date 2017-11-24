68c214ffb5ac90e10a476bbf3528eb346cf08007
Ramon Jimenez Pomareta <ramonjosejimenezpomareta@gmail.com>
2017-11-24 15:29:36 +0100

    delete events

---
+++ tree.js
@@ -0,0 +1,139 @@
+function Queue() {
+    this._oldestIndex = 1;
+    this._newestIndex = 1;
+    this._storage = {};
+}
+ 
+Queue.prototype.size = function() {
+    return this._newestIndex - this._oldestIndex;
+};
+ 
+Queue.prototype.enqueue = function(data) {
+    this._storage[this._newestIndex] = data;
+    this._newestIndex++;
+};
+ 
+Queue.prototype.dequeue = function() {
+    var oldestIndex = this._oldestIndex,
+        newestIndex = this._newestIndex,
+        deletedData;
+ 
+    if (oldestIndex !== newestIndex) {
+        deletedData = this._storage[oldestIndex];
+        delete this._storage[oldestIndex];
+        this._oldestIndex++;
+ 
+        return deletedData;
+    }
+};
+
+function Node(id, name) {
+    this.id = id;
+    this.name = name;
+    this.parent = null;
+    this.children = [];
+}
+ 
+function Tree(id, name) {
+    var node = new Node(id, name);
+    this._root = node;
+}
+ 
+Tree.prototype.traverseDF = function(callback) {
+ 
+    // this is a recurse and immediately-invoking function
+    (function recurse(currentNode) {
+        // step 2
+        for (var i = 0, length = currentNode.children.length; i < length; i++) {
+            // step 3
+            recurse(currentNode.children[i]);
+        }
+ 
+        // step 4
+        callback(currentNode);
+ 
+        // step 1
+    })(this._root);
+ 
+};
+ 
+Tree.prototype.traverseBF = function(callback) {
+    var queue = new Queue();
+ 
+    queue.enqueue(this._root);
+ 
+    currentTree = queue.dequeue();
+ 
+    while(currentTree){
+        for (var i = 0, length = currentTree.children.length; i < length; i++) {
+            queue.enqueue(currentTree.children[i]);
+        }
+ 
+        callback(currentTree);
+        currentTree = queue.dequeue();
+    }
+};
+ 
+Tree.prototype.contains = function(callback, traversal) {
+    traversal.call(this, callback);
+};
+ 
+Tree.prototype.add = function(id, name, parentID, traversal) {
+    var child = new Node(id, name),
+        parent = null,
+        callback = function(node) {
+            if (node.id === parentID) {
+                parent = node;
+            }
+        };
+ 
+    this.contains(callback, traversal);
+ 
+    if (parent) {
+        parent.children.push(child);
+        child.parent = parent;
+    } else {
+        throw new Error('Cannot add node to a non-existent parent.');
+    }
+};
+ 
+Tree.prototype.remove = function(id, parentID, traversal) {
+    var tree = this,
+        parent = null,
+        childToRemove = null,
+        index;
+ 
+    var callback = function(node) {
+        if (node.id === parentID) {
+            parent = node;
+        }
+    };
+ 
+    this.contains(callback, traversal);
+ 
+    if (parent) {
+        index = findIndex(parent.children, id);
+ 
+        if (index === undefined) {
+            throw new Error('Node to remove does not exist.');
+        } else {
+            childToRemove = parent.children.splice(index, 1);
+        }
+    } else {
+        throw new Error('Parent does not exist.');
+    }
+ 
+    return childToRemove;
+};
+ 
+function findIndex(arr, id) {
+    var index;
+ 
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].id === id) {
+            index = i;
+        }
+    }
+ 
+    return index;
+}
