25af8e4113eb078281f2c407212aafb94f9a584a
Ramon Jose Jimenez Pomareta <ramonjosejimenezpomareta@gmail.com>
2018-01-19 15:58:49 +0100

    working copy

---
+++ excelImport.js
@@ -0,0 +1,137 @@
+/***********
+ * 
+ * This file contains function to import Excel content
+ * 
+ * @author Ramón José Jiménez Pomareta
+ * @version 1
+ * @date 19.01.2018
+ * 
+ * 
+ * requires on main 
+ *          function processSheet(array, sheetNo)
+ */       
+
+
+
+    
+            /*jshint browser:true */
+        /* eslint-env browser */
+        /* eslint no-use-before-define:0 */
+        /*global Uint8Array, Uint16Array, ArrayBuffer */
+        /*global XLSX */
+        var X = XLSX;
+        var XW = {
+            /* worker message */
+            msg: 'xlsx',
+            /* worker scripts */
+            worker: './xlsxworker.js'
+        };
+        
+        
+        var process_wb = (function() {
+
+            
+            var get_format = (function() {
+                var radios = document.getElementsByName( "format" );
+                return function() {
+                    for(var i = 0; i < radios.length; ++i) if(radios[i].checked || radios.length === 1) return radios[i].value;
+                };
+            })();
+        
+            var to_array = function to_array(workbook) {
+                var result = {};
+                var i = 0;
+                workbook.SheetNames.forEach(function(sheetName) {
+                    var roa = workbook.Sheets[sheetName];
+                    if(typeof roa !== 'undefined') result[i] = roa;
+                    i++;
+                });
+                return result;
+            };
+        
+            return function process_wb(wb) {
+                var output = to_array(wb);
+                for (var i = 0; i<output.length; i++){
+                    processSheet(output[i], i);
+                }
+
+            };
+        })();
+        
+        
+      
+       
+       var do_file = (function() {
+            var rABS = typeof FileReader !== "undefined" && (FileReader.prototype||{}).readAsBinaryString;
+            var domrabs = document.getElementsByName("userabs")[0];
+            if(!rABS) domrabs.disabled = !(domrabs.checked = false);
+        
+            //var use_worker = typeof Worker !== 'undefined';
+            var domwork = document.getElementsByName("useworker")[0];
+            //if(!use_worker) domwork.disabled = !(domwork.checked = false);
+        
+            var xw = function xw(data, cb) {
+                var worker = new Worker(XW.worker);
+                worker.onmessage = function(e) {
+                    switch(e.data.t) {
+                        case 'ready': break;
+                        case 'e': console.error(e.data.d); break;
+                        case XW.msg: cb(JSON.parse(e.data.d)); break;
+                    }
+                };
+                worker.postMessage({d:data,b:rABS?'binary':'array'});
+            };
+        
+            return function do_file(files) {
+                //use_worker = domwork.checked;
+                var f = files[0];
+                var reader = new FileReader();
+                reader.onload = function(e) {
+                    if(typeof console !== 'undefined') console.log("onload", new Date(), rABS);
+                    var data = e.target.result;
+                    if(!rABS) data = new Uint8Array(data);     
+                    process_wb(X.read(data, {type: rABS ? 'binary' : 'array'}));
+                };
+                if(rABS) reader.readAsBinaryString(f);
+                else reader.readAsArrayBuffer(f);
+            };
+        })();
+        
+        
+        // prepares behaviour for dropping files
+        (function() {
+            $(".drop")
+     		.on("drop", function(e) {
+                e.preventDefault();
+                $(".drop").switchClass( "dropInverse", "drop");
+                if (e.currentTarget.id == "drop"){
+                    do_file(e.originalEvent.dataTransfer.files);
+                }/* else if (e.currentTarget.id == "drop6"){
+                    prepareFile("fileResources", e.originalEvent.dataTransfer.files[0], sendFile, associateNewFile, updateAvailableFiles);
+                }*/
+
+            })
+            .on("dragenter", function (e) {
+                e.preventDefault();
+                $(".drop").switchClass( "drop", "dropInverse");
+                e.originalEvent.dataTransfer.dropEffect = 'copy';
+            })
+            .on("dragleave", function (e) {
+                $(".drop").switchClass( "dropInverse", "drop");
+            })            
+            .on('dragover',function(e){
+                  e.preventDefault();
+            });
+        })();
+        
+        // prepares behaviour for importing files
+        (function() {
+            var xlf = document.getElementById('import_end');
+            if(!xlf.addEventListener) return;
+            function handleFile(e) { 
+                do_file(e.target.files); 
+                $("#uploadFilesForm")[0].reset();
+            }
+            xlf.addEventListener('change', handleFile, false);
+            
+        })();
---
+++ inputControl.js
@@ -0,0 +1,134 @@
+/***********
+ * 
+ * This file contains function that controls the input of the user
+ * 
+ * @author Ramón José Jiménez Pomareta
+ * @version 1
+ * @date 19.01.2018
+ * @requires jQuery-UI
+ */
+
+var excelHeaders = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "AA", "AB", "AC", "AD", "AE", "AF", "AG", "AH", "AI", "AJ", "AK", "AL", "AM", "AN", "AO", "AP", "AQ", "AR", "AS", "AT", "AU", "AV", "AW", "AX", "AY", "AZ", "BA", "BB", "BC", "BD", "BE", "BF", "BG", "BH"];
+var ASC = 1;
+var DESC = 2;
+
+/**
+* Generates a list of number from next year until 1900 and stores in generatedYears
+*/
+function generateNumbers(from, to, asc){
+    var numbers = [];
+    var number;
+
+    if(ASC == asc){
+        for (i = from; i <= to; i++)
+        {
+            number = new Object();
+            number.value = i;
+            number.label = i;
+            numbers.push(number);
+        }
+    } else {
+        for (i = to; i >= from; i--)
+        {
+            number = new Object();
+            number.value = i;
+            number.label = i;
+            numbers.push(number);
+        }
+    }
+
+    return numbers;
+}
+
+function distanceBetween(letter1, letter2){
+	var count = 0;
+
+	while (letter1.toLowerCase().localeCompare(letter2.toLowerCase()) < 0 || letter1.length < letter2.length){
+		letter1 = nextLetter(letter1);
+		count ++;
+	}
+	return count;
+}
+
+/**
+ * Return next letter. A->B, AB->AC, Z->AA
+ * @param {string} key a string [a-zA-Z]+
+ */
+
+function nextLetter (key) {
+  if (key === 'Z' || key === 'z') {
+    return String.fromCharCode(key.charCodeAt() - 25) + String.fromCharCode(key.charCodeAt() - 25); // AA or aa
+  } else {
+    var lastChar = key.slice(-1);
+    var sub = key.slice(0, -1);
+    if (lastChar === 'Z' || lastChar === 'z') {
+      // If a string of length > 1 ends in Z/z,
+      // increment the string (excluding the last Z/z) recursively,
+      // and append A/a (depending on casing) to it
+      return nextLetter(sub) + String.fromCharCode(lastChar.charCodeAt() - 25);
+    } else {
+      // (take till last char) append with (increment last char)
+      return sub + String.fromCharCode(lastChar.charCodeAt() + 1);
+    }
+  }
+  return key;
+};
+
+function autocompleteSearcher(elementId, dataSource, callbackFunction, minLength){
+    $( "#"+elementId ).autocomplete({
+        source: dataSource,
+        select: function(event, ui) {
+            event.preventDefault();
+            $("#"+elementId).val(ui.item.label);
+            $("#"+elementId).text(ui.item.value);
+            callbackFunction(ui.item);
+        },
+        focus: function(event, ui) {
+            event.preventDefault();
+            $("#"+elementId).val(ui.item.label);
+            $("#"+elementId).text(ui.item.value);
+
+        },
+        change: function (event, ui) {
+            if(!ui.item){
+                //http://api.jqueryui.com/autocomplete/#event-change -
+                // The item selected from the menu, if any. Otherwise the property is null
+                //so clear the item for force selection
+                $("#"+elementId).val("");
+                $("#"+elementId).text("");
+            }
+
+        },
+        delay: 500,
+        minLength: minLength
+    });
+}
+
+
+    // Modifies the autocomplete filter in order to match also other fields than label
+$.ui.autocomplete.filter = function (array, term) {
+    var matcher = new RegExp('(^| )' + $.ui.autocomplete.escapeRegex(term), 'i');
+    return $.grep(array, function (value) {
+        return matcher.test(value.label) || matcher.test(value.value) || matcher.test(value.code) || matcher.test(value.name) || matcher.test(value);
+    });
+};
+
+
+function getArrayOfLetters(letter1, letter2){
+    var array = [];
+    
+    if (letter1.match(/[a-z]/i) && letter2.match(/[a-z]/i)) {
+        // alphabet letters found
+        while (letter1.toLowerCase().localeCompare(letter2.toLowerCase()) < 0 || letter1.length < letter2.length){
+            array.push(letter1);
+            letter1 = nextLetter(letter1);
+        }
+        array.push(letter1);
+    }
+
+
+    
+    return array;
+}
+
+
